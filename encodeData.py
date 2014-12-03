31cab145d70df35db37331b3a72239d390fbb260
Fernando S <fjsapereira@yahoo.ca>
2014-12-02 23:38:03 -0500

    loop through libraries
    now instead of running the library with the branch name hard-coded, the
    pgm will loop through a list of 8 branches and report results for each

--- encodeData.py
+++ encodeData.py
@@ -42,6 +42,24 @@ def circInfo(data):
             
     return authDict
     
+def titleInfo(data):
+    keywordsDict = {}
+    for d in data:
+        for word in d.title:
+            if word in keywordsDict:
+                wordCounts = keywordsDict[word]
+            else:
+                wordCounts = [0,0]        
+                
+            if d.circ >= threshold:
+                wordCounts[0] = wordCounts[0]+1
+            else:
+                wordCounts[1] = wordCounts[1]+1
+    
+                keywordsDict[word] = wordCounts
+            
+    return keywordsDict
+    
 def loadStops(filename):
     """
     stop words list source: http://www.textfixer.com/resources/common-english-words.txt
@@ -75,141 +93,145 @@ stopword list source: http://www.textfixer.com/resources/common-english-words.tx
 """
 stopwords = [loadStops('eng-stopwords.csv'), loadStops('fr-stopwords.csv')]
 
-skipped = 0
-dupl = 0
-exc=[0,0,0]
-
-loans = {}
-
-with open('nonfiction-no-accents.csv','rb') as infile:
-    datareader = csv.reader(infile)
-    for record in datareader:
-       if record[0] == "Sans Arroundissement": 
-                skipflag = False
-            
-                callno = re.match('\D*([\d\.]*)',record[7]).group(1)
-                if callno != '':
-                    try:
-                        callno = float(callno)
-                    except ValueError:
-                        callno = 0
-                        skipFlag = True
-                else:
-                    exc[0]=exc[0]+1
-                    skipflag = True 
-
-                circ = int(record[3])
+for library in ["Saint-Laurent","Plateau-Mont-Royal","Saint-Leonard",\
+                "Outremont","Anjou","Mercier-Hochelaga-Maisonneuve","Verdun",\
+                "Sans Arroundissement"]:
+    print "\n\nResults for",library    
+    skipped = 0
+    dupl = 0
+    exc=[0,0,0]
+    
+    loans = {}
+    
+    with open('nonfiction-no-accents.csv','rb') as infile:
+        datareader = csv.reader(infile)
+        for record in datareader:
+           if record[0] == library: 
+                    skipflag = False
                 
-                author = authorInfo(record[10])
-                if author == None:
-                    exc[1]=exc[1]+1
-                    skipflag = True 
-           
-                m=re.search("\D*(\d{4})",record[15])
-                if record[15] != '':
-                    if m == None or m.group(1) == '':
-                        exc[2]=exc[2]+1
+                    callno = re.match('\D*([\d\.]*)',record[7]).group(1)
+                    if callno != '':
+                        try:
+                            callno = float(callno)
+                        except ValueError:
+                            callno = 0
+                            skipFlag = True
+                    else:
+                        exc[0]=exc[0]+1
                         skipflag = True 
-                    else:
-                        year = int(m.group(1))
-                
-                p = re.search("\D*(\d*)\D*",record[16])
-                if p != None and p.group(1) != '':
-                    pages = int(p.group(1))
+    
+                    circ = int(record[3])
                     
-                if record[17] == 'eng':
-                    lang = 0
-                elif record[17] == 'fre':
-                    lang = 1
-                else:
-                    skipflag = True 
-           
-                title = reduce(record[8],stopwords[lang])
-                title = title + reduce(record[9],stopwords[lang])
- 
-                if not skipflag:
-                    book = Book()
-                    bookkey=(callno,author,year,lang)
-                    if bookkey in loans:
-                        loans[bookkey].circ = loans[bookkey].circ + circ
-                        dupl = dupl +1
+                    author = authorInfo(record[10])
+                    if author == None:
+                        exc[1]=exc[1]+1
+                        skipflag = True 
+               
+                    m=re.search("\D*(\d{4})",record[15])
+                    if record[15] != '':
+                        if m == None or m.group(1) == '':
+                            exc[2]=exc[2]+1
+                            skipflag = True 
+                        else:
+                            year = int(m.group(1))
+                    
+                    p = re.search("\D*(\d*)\D*",record[16])
+                    if p != None and p.group(1) != '':
+                        pages = int(p.group(1))
+                        
+                    if record[17] == 'eng':
+                        lang = 0
+                    elif record[17] == 'fre':
+                        lang = 1
                     else:
-                        book.callno = callno
-                        book.circ = circ
-                        book.author = author
-                        book.title = title
-                        book.year = year
-                        book.pages = pages
-                        book.lang = lang
-                        book.isbn = record[18]
-                        loans[bookkey]=book                  
-                else:
-                    skipped = skipped + 1 
-
-
-infile.close()
-print skipped, exc 
-print 'duplicate books ',dupl 
-print 'unique books ', len(loans) 
-
-for m in range(10):
-    i = 0
-    trainSet = []
-    testSet =[]
-    for book in loans.values():
-        i = i + 1
-        if i % 10 != m:
-            trainSet.append(book)
-        else:
-            testSet.append(book)
+                        skipflag = True 
+               
+                    title = reduce(record[8],stopwords[lang])
+                    title = title + reduce(record[9],stopwords[lang])
+     
+                    if not skipflag:
+                        book = Book()
+                        bookkey=(callno,author,year,lang)
+                        if bookkey in loans:
+                            loans[bookkey].circ = loans[bookkey].circ + circ
+                            dupl = dupl +1
+                        else:
+                            book.callno = callno
+                            book.circ = circ
+                            book.author = author
+                            book.title = title
+                            book.year = year
+                            book.pages = pages
+                            book.lang = lang
+                            book.isbn = record[18]
+                            loans[bookkey]=book                  
+                    else:
+                        skipped = skipped + 1 
     
-    authDict = circInfo(trainSet)
     
-    x = []
-    y = []
-    for book in trainSet:
-        if book.author[0] == '':
-            circ = [0,0]
-        else:
-            circ = authDict[book.author]
-        x.append([book.callno,circ[0], circ[1],book.year,book.lang])
-        if book.circ > threshold:
-            y.append(1)
-        else:
-            y.append(0)
+    infile.close()
+    print skipped, exc 
+    print 'duplicate books ',dupl
+    print '# distinct books',len(loans)
+    
+    for m in range(10):
+        i = 0
+        trainSet = []
+        testSet =[]
+        for book in loans.values():
+            i = i + 1
+            if i % 10 != m:
+                trainSet.append(book)
+            else:
+                testSet.append(book)
         
-    from sklearn import neighbors
-    n_neighbors = 20
-    
-    clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance')
-    clf.fit(x, y)
-    
-    x=[]
-    y=[]
-    for book in testSet:
-        if book.author in authDict:
-            circ = authDict[book.author]
-        else:
-            circ = [0,0]
-        x.append([book.callno,circ[0], circ[1],book.year,book.lang])
-        if book.circ > threshold:
-            y.append(1)
-        else:
-            y.append(0)
-    
-    success = 0
-    failure = 0
-    results=[]
-    for i in range(len(x)):
-        Z=clf.predict(x[i])
-        results.append((x[i],Z,y[i],Z==y[i]))
-        if Z==y[i]:
-            success = success + 1
-        else:
-            failure = failure + 1
-    print float(success)/(success+failure)
-    with open('results'+'{}'.format(m)+'.csv','w') as csvfile:
-        writer = csv.writer(csvfile,delimiter = ',', quotechar='\"', quoting=csv.QUOTE_ALL)
-        for r in results:
-            writer.writerow(r)
-    csvfile.close()
+        authDict = circInfo(trainSet)
+        titleKeyWords = titleInfo(trainSet)
+        x = []
+        y = []
+        for book in trainSet:
+            if book.author[0] == '':
+                circ = [0,0]
+            else:
+                circ = authDict[book.author]
+            x.append([book.callno,circ[0], circ[1],book.year,book.lang])
+            if book.circ > threshold:
+                y.append(1)
+            else:
+                y.append(0)
+            
+        from sklearn import neighbors
+        n_neighbors = 20
+        
+        clf = neighbors.KNeighborsClassifier(n_neighbors)
+        clf.fit(x, y)
+        
+        x=[]
+        y=[]
+        for book in testSet:
+            if book.author in authDict:
+                circ = authDict[book.author]
+            else:
+                circ = [0,0]
+            x.append([book.callno,circ[0], circ[1],book.year,book.lang])
+            if book.circ > threshold:
+                y.append(1)
+            else:
+                y.append(0)
+        
+        success = 0
+        failure = 0
+        results=[]
+        for i in range(len(x)):
+            Z=clf.predict(x[i])
+            results.append((x[i],Z,y[i],Z==y[i]))
+            if Z==y[i]:
+                success = success + 1
+            else:
+                failure = failure + 1
+        print float(success)/(success+failure)
+        with open('results'+'{}'.format(m)+'.csv','w') as csvfile:
+            writer = csv.writer(csvfile,delimiter = ',', quotechar='\"', quoting=csv.QUOTE_ALL)
+            for r in results:
+                writer.writerow(r)
+        csvfile.close()
